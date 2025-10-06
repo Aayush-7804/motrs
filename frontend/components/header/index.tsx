62b1a8f4f568b3197d6c99b6313fd8c4561ef8cf
Aayush-7804 <aayushranapariya@gmail.com>
2025-10-06 18:02:48 +0530

    Page: Header component responsive modification

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -16,8 +16,8 @@ const navItems: string[] = [
 const Header = () => {
   return (
     <header className="bg-[var(--Primary-Midnight-Blue)] text-[var(--other-White)]">
-      <div className="max-w-360 h-25 px-10 py-4.75 mx-auto flex justify-between items-center">
-        <div className="min-[769px]:hidden px-14.5">
+      <div className="max-w-360 h-25 px-10 py-5 max-[376px]:px-5 max-[376px]:py-6.25 mx-auto flex justify-between items-center">
+        <div className="min-[769px]:hidden px-14.5 max-[376px]:px-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="17"
@@ -64,11 +64,28 @@ const Header = () => {
           </ul>
         </nav>
         <div className="flex items-center gap-9.75">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="23"
+              height="23"
+              viewBox="0 0 23 23"
+              fill="none"
+            >
+              <path
+                d="M17.7288 17.7292L20.1247 20.125M3.83301 5.75H19.1663M3.83301 11.5H7.66634M3.83301 17.25H7.66634M10.5413 14.375C10.5413 15.3917 10.9452 16.3667 11.6641 17.0856C12.383 17.8045 13.358 18.2083 14.3747 18.2083C15.3913 18.2083 16.3664 17.8045 17.0852 17.0856C17.8041 16.3667 18.208 15.3917 18.208 14.375C18.208 13.3583 17.8041 12.3833 17.0852 11.6644C16.3664 10.9455 15.3913 10.5417 14.3747 10.5417C13.358 10.5417 12.383 10.9455 11.6641 11.6644C10.9452 12.3833 10.5413 13.3583 10.5413 14.375Z"
+                stroke="#E2003C"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
           <div className="max-[1025px]:hidden flex items-center gap-6 text-[var(--other-offWhite)]">
             <Magnifying />
             <RegHeart />
           </div>
-          <Button className="bg-[var(--Primary-Cheery-Red)]">
+          <Button className="max-[376px]:hidden bg-[var(--Primary-Cheery-Red)]">
             sign Up/ Login
           </Button>
         </div>
