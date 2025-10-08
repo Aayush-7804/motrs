6548519ff4fade191243abdf3624b10609c8573a
Aayush-7804 <aayushranapariya@gmail.com>
2025-10-08 11:24:52 +0530

    Page: Header component modification

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -64,7 +64,7 @@ const Header = () => {
           </ul>
         </nav>
         <div className="flex items-center gap-9.75">
-          <div>
+          <div className="min-[769px]:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="23"
